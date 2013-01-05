65ccbfa414e7746c88149d316d35f9a8d0a43c78
Jitlee.Wan <www.wpj@163.com>
2013-01-06 02:04:18 +0800

    ....

---
+++ Avmt.Test/Avmt.Test/IsVisibleChangedEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Avmt.Test
+{
+    internal class IsVisibleChangedEventArgs : EventArgs
+    {
+        private bool _isVisible;
+        public bool IsVisible { get { return _isVisible; } }
+
+        public IsVisibleChangedEventArgs(bool isVisible)
+        {
+            _isVisible = isVisible;
+        }
+    }
+}
