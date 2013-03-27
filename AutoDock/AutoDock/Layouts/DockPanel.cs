4b18ea146c5585be1dbe5b30e547c977c98034ee
AB <www.wpj@163.com>
2013-03-27 18:14:50 +0800

    Signed-off-by: AB <www.wpj@163.com>

---
+++ AutoDock/AutoDock/Layouts/DockPanel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Markup;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace AutoDock.Layouts
+{
+    [ContentProperty("Children")]
+    [TemplatePart(Name="Part_Canvas", Type= typeof(Canvas))]
+    public class DockPanel : Control
+    {
+
+        private readonly Collection<Layout> _children = new Collection<Layout>();
+
+        public Collection<Layout> Children { get { return _children; } }
+
+        static DockPanel()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(DockPanel),
+                new FrameworkPropertyMetadata(
+                    typeof(DockPanel)));
+        }
+
+        public DockPanel()
+        {
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+        }
+    }
+}
